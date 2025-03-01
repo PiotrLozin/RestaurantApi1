60518700ec6dfe69b91d3c2677fe33bddc9c510f
PiotrLozin <63423136+PiotrLozin@users.noreply.github.com>
2025-03-01 13:49:15 +0100

    Created a simple Database

---
+++ Entities/RestaurantDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantApi.Entities
+{
+    public class RestaurantDbContext : DbContext
+    {
+        private string _connectionString = 
+            "Server=(localdb)\\mssqllocaldb;Database=RestaurantDb;Trusted_Connection=True;";
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Dish> Dishes { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+
+        public RestaurantDbContext()
+        { }
+        public RestaurantDbContext(DbContextOptions<RestaurantDbContext> options) : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Restaurant>()
+                .Property(r => r.Name)
+                .IsRequired()
+                .HasMaxLength(25);
+
+            modelBuilder.Entity<Dish>()
+                .Property(d => d.Name)
+                .IsRequired();
+        }
+
+        override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
