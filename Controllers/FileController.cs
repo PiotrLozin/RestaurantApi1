1ffeb845b6adef6d511b7f121c1238436d43748d
PiotrLozin <63423136+PiotrLozin@users.noreply.github.com>
2025-04-05 17:15:17 +0200

    Added file Upload and Cache

--- Controllers/FileController.cs
+++ Controllers/FileController.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.StaticFiles;
 
 namespace RestaurantApi.Controllers
 {
     [Route("file")]
-    [Authorize]
+    // [Authorize]
     public class FileController : ControllerBase
     {
+        [HttpGet]
+        [ResponseCache(Duration = 1200, VaryByQueryKeys = new[] {"fileName"})]
         public ActionResult GetFile([FromQuery] string fileName)
         {
             var rootPath = Directory.GetCurrentDirectory();
@@ -24,6 +28,24 @@ namespace RestaurantApi.Controllers
             var fileContents = System.IO.File.ReadAllBytes(filePath);
 
             return File(fileContents, contentType, fileName);
+        }
+
+        [HttpPost]
+        public ActionResult Upload([FromForm] IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+                var rootPath = Directory.GetCurrentDirectory();
+                var fullPath = $"{rootPath}/PrivateFiles/{file.FileName}";
+                using (var stream = new FileStream(fullPath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+
+                return Ok();
+            }
+
+            return BadRequest();
         }
     }
 }
